9adc29720bc99771910590e33660195ff9080b4f
RAMESH <ramesh1248b@gmail.co>
2023-12-13 14:28:19 +0530

    JPA Equals

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
-import org.hibernate.mapping.Set;
+
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Author {
@@ -45,5 +47,30 @@ public class Author {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
