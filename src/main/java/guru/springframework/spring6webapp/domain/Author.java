6bd9013c05724e36e8e84884bd543faa6d6b9c5b
RAMESH <ramesh1248b@gmail.co>
2023-12-14 02:15:02 +0530

    Misc Update

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -3,7 +3,6 @@ package guru.springframework.spring6webapp.domain;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -16,7 +15,7 @@ public class Author {
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books = new HashSet<>();;
+    private Set<Book> books = new HashSet<>();
 
     public Long getId() {
         return id;
